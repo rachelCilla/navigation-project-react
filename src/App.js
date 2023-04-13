e5347f96a0b1efabe6829a614beb70ad9135e945
appaandbuki <104958634+rachelCilla@users.noreply.github.com>
2023-04-13 13:05:38 -0600

    dropdown component setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const items = [
-    {
-      id: 1,
-      label: "What is React?",
-      content: "React is a front end javascript framework",
-    },
-    {
-      id: 2,
-      label: "Why use React?",
-      content: "React is a favorite JS library among engineers",
-    },
-    {
-      id: 3,
-      label: "How do you use React?",
-      content: "You use React by creating components",
-    },
-  ];
-
   return (
     <div>
-      <Accordion items={items} />
+      <Dropdown />
     </div>
   );
 }
